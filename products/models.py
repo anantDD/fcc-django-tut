76b0082d7e12221ad02a316608aa6dfc7cdd4771
anant <anant@anant.com>
2019-09-18 17:45:37 +0530

    dynamic linking of urls

--- products/models.py
+++ products/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 
@@ -9,3 +10,8 @@ class Product(models.Model):
     price = models.DecimalField(decimal_places=2, max_digits=10000)
     summary = models.TextField(blank=False, null=False)
     featured = models.BooleanField(default=False)
+
+    # def get_absolute_url(self):
+    #     return reverse("product", kwargs={"my_id": self.id})
+    def get_absolute_url(self):
+        return f"/products/{self.id}/"
