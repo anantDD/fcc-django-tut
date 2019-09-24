cd7707cf67609d423bb8d815f9e98397e54f84c4
anant <anant@anant.com>
2019-09-24 21:24:54 +0530

    ListView, DetailView

--- blogs/urls.py
+++ blogs/urls.py
@@ -3,11 +3,15 @@ from django.urls import path
 from .views import (
     get_all_articles_view,
     get_article_view,
-    create_article_view
+    create_article_view,
+    ArticleListView,
+    ArticleDetailView
 )
 
 urlpatterns = [
+    path('l', ArticleListView.as_view(), name='list'),
     path('', get_all_articles_view, name='list'),
     path('create/', create_article_view, name='create'),
-    path('article/<int:id>', get_article_view, name='article-detail')
+    path('article/<int:id>', ArticleDetailView.as_view(), name='detail')
+    # path('article/<int:id>', get_article_view, name='article-detail')
 ]
--- blogs/views.py
+++ blogs/views.py
@@ -2,7 +2,28 @@ from django.shortcuts import render, get_object_or_404
 from .forms import ArticleForm
 from .models import Article
 
+from django.views.generic import (
+    ListView,
+    DetailView,
+)
+
 # Create your views here.
+
+
+class ArticleListView(ListView):
+    template_name = "blogs/article_list1.html"
+    queryset = Article.objects.all()
+
+
+class ArticleDetailView(DetailView):
+    # queryset = Article.objects.filter(id=2)
+    # print(queryset)
+
+    def get_object(self):
+        print(self.kwargs)
+        print('therse')
+        _id = self.kwargs.get("id")
+        return get_object_or_404(Article, id=_id)
 
 
 def create_article_view(request):
@@ -34,4 +55,4 @@ def get_all_articles_view(request):
         'object_list': object_list
     }
 
-    return render(request, "blogs/article_list.html", context)
+    return render(request, "blogs/article_list1.html", context)
